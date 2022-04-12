aa8e3c1b894c7d5456eb112422c4050090cfa742
ia <isaac.ardis@gmail.com>
2022-04-12 09:02:26 -0700

    :floppy_disk:
    Signed-off-by: ia <isaac.ardis@gmail.com>

--- pushup.sh
+++ pushup.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+set -e
 set -x
 
 # build:dist doesnt work, so we build dev
 npm run build:dist
 
-rsync -avz ./dist/ rottor:/root/riverlevel.ardis.space/
+rsync -avz --delete ./dist/ rottor:/root/riverlevel.ardis.space/
